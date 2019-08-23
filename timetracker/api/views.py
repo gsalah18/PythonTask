cb3a38c42a5c3826db7b5f18ed5d5beb538a52bb
Mohannad Abusafieh <mabusafieh@liveareacx.com>
2019-08-24 02:17:11 +0300

    Unit test is ready

--- timetracker/api/views.py
+++ timetracker/api/views.py
@@ -59,10 +59,16 @@ class EmployeeViewSet(ModelViewSet):
     @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated],
             url_path='set-vacation', url_name='set_vacation')
     def set_vacation(self, request, pk=None):
+        try:
+            if ('date' in request.data):
+                datetime.strptime(request.data['date'], '%Y-%m-%d')
+        except ValueError:
+            return Response({'error': 'Date is invalid'}, status=status.HTTP_400_BAD_REQUEST)
         vacation_serializer = VacationSerializer(
             data={'employee': pk,
                   'description': request.data['description'] if 'description' in request.data else '',
-                  'date': datetime.strptime(request.data['date'], '%Y-%m-%d').date() if 'date' in request.data else None})
+                  'date': datetime.strptime(request.data['date'],'%Y-%m-%d').date()
+                  if 'date' in request.data else None})
         if vacation_serializer.is_valid():
             vacation_serializer.save()
             return Response({'result': vacation_serializer.data, 'error': None}, status=status.HTTP_201_CREATED)
@@ -79,11 +85,14 @@ class EmployeeViewSet(ModelViewSet):
             return Response({'error': 'Employee is not found for id {}'.format(pk)}, status=status.HTTP_404_NOT_FOUND)
         check_in_times = employee.checking_set.filter(check='in').values_list('time')
         check_out_times = employee.checking_set.filter(check='out').values_list('time')
+        if (check_in_times.count() == 0 or check_out_times.count() == 0):
+            return Response({'error': 'There is no checkout Data'}, status=status.HTTP_400_BAD_REQUEST)
+
         arrival_avg_time = self.get_dates_avg(check_in_times)
         leaving_avg_time = self.get_dates_avg(check_out_times)
         return Response({'result': {'arrival_time': '{}:{}'.format(arrival_avg_time.hour, arrival_avg_time.minute),
                                     'leaving_time': '{}:{}'.format(leaving_avg_time.hour, leaving_avg_time.minute)},
-                         'error': None}, status=status.HTTP_200_OK)
+                                    'error': None}, status=status.HTTP_200_OK)
 
     '''
         *period value pattern is pipe seperated:
@@ -105,51 +114,52 @@ class EmployeeViewSet(ModelViewSet):
         if period_value is None or period_type is None:
             return Response({'error': 'Either period_value or period_value type is missing'},
                             status=status.HTTP_400_BAD_REQUEST)
-
+        emp_working_hours = None
         if period_type == 'year':
-            if not (self.is_a_number(period_value)):
-                Response({'error': 'period value is invalid, year input should be only digits, ex:2019'},
+            try:
+                int(period_value)
+            except ValueError:
+                return Response({'error': 'period value is invalid, year input should be only digits, ex:2019'},
                          status=status.HTTP_400_BAD_REQUEST)
-            emp_working_hours = employee.workinghours_set.all().filter(date__range=period_value).aggregate(Sum('hours'))
+            if employee.workinghours_set.all().count() > 0:
+                emp_working_hours = employee.workinghours_set.all().filter(date__year=period_value).aggregate(
+                    Sum('hours'))
 
         elif period_type == 'quarter':
             if len(period_value.split('|')) != 2:
-                Response(
+                return Response(
                     {'error': 'period value is invalid, quarter input should be year piped by quarter, ex: 2019|3'},
                     status=status.HTTP_400_BAD_REQUEST)
             year = period_value.split('|')[0]
             quarter = period_value.split('|')[1]
-            if not (self.is_a_number(year)) or not (self.is_a_number(quarter)) or not (1 <= int(quarter) <= 4):
-                Response(
+            try:
+                int(year)
+                int(quarter)
+                if not (1 <= int(quarter) <= 4):
+                    raise ValueError()
+            except ValueError:
+                return Response(
                     {'error': 'period value is invalid, quarter input should be year piped by quarter, ex: 2019|3'},
                     status=status.HTTP_400_BAD_REQUEST)
-            emp_working_hours = employee.workinghours_set.all().filter(
-                date__range=['{}-{}-01'.format(year, self.YEAR_QUARTERS[
-                    quarter][0]),
-                             '{}-{}-01'.format(year,
-                                               self.YEAR_QUARTERS[
-                                                   quarter][
-                                                   1])]).aggregate(Sum('hours'))
+            if employee.workinghours_set.all().count() > 0:
+                emp_working_hours = employee.workinghours_set.all().filter(
+                    date__range=['{}-{}-01'.format(year, self.YEAR_QUARTERS[quarter][0]),
+                                 '{}-{}-01'.format(year,self.YEAR_QUARTERS[quarter][1])]).\
+                                    aggregate(Sum('hours'))
 
         elif period_type == 'week':
             try:
                 date_value = datetime.strptime(period_value, '%Y-%m-%d')
             except ValueError:
-                Response({'error': 'period value is invalid, week input should be date dash seperated, ex: 2019-3-18'},
+                return Response({'error': 'period value is invalid, week input should be date dash seperated, ex: 2019-3-18'},
                          status=status.HTTP_400_BAD_REQUEST)
-            emp_working_hours = employee.workinghours_set.all().filter(date__range=[date_value,
-                                                                                    date_value + timedelta(
-                                                                                        weeks=1)]).aggregate(
-                Sum('hours'))
+            if employee.workinghours_set.all().count() > 0:
+                emp_working_hours = employee.workinghours_set.all().\
+                    filter(date__range=[date_value,date_value + timedelta(weeks=1)]).aggregate(Sum('hours'))
 
-        else:
-            emp_working_hours = None
         if emp_working_hours is None:
             return Response({'error': 'prod type is not valid'}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'result': emp_working_hours['hours__sum'], 'error': None}, status=status.HTTP_200_OK)
-
-    def is_a_number(self, str):
-        return any(char.isdigit() for char in str)
 
     def get_dates_avg(self, dates):
         sum = 0
@@ -168,6 +178,9 @@ I hope it does make sense :/.
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_team_work_percent(request):
+    if WorkingHours.objects.all().count() == 0:
+        return Response({'error': 'There is no work hours data'}, status=status.HTTP_400_BAD_REQUEST)
+
     team_work_hours_sum = WorkingHours.objects.all().aggregate(Sum('hours'))
     total_hours = WorkingHours.objects.all().count() * 8
     team_work_percent = team_work_hours_sum['hours__sum'] / total_hours * 100
